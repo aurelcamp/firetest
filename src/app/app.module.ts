656caaf65e3129d6af81c006933a25670079bf20
aurelcamp <aureliencampistron@yahoo.fr>
2021-11-08 12:33:48 +0100

    change firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+// import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+// import { environment } from '../environments/environment';
+// import { provideAuth,getAuth } from '@angular/fire/auth';
+// import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +21,8 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
     // provideFirebaseApp(() => initializeApp(environment.firebase)),
     // provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ],
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-// import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 // import * as firebase from 'firebase';
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-import { collection, getDocs } from "firebase/firestore"; 
+// import { initializeApp } from "firebase/app"
+// import { getFirestore } from "firebase/firestore"
+// import { collection, getDocs } from "firebase/firestore"; 
 
 @Component({
   selector: 'app-folder',
@@ -19,36 +19,43 @@ export class FolderPage implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    // private firestore: Firestore
+    private firestore: AngularFirestore
   ) { }
 
   async ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.firestore.collection('users').valueChanges().subscribe(users => {
+      console.log(users);
+      this.users = users;
+    })
+
+
     // const col = collection(this.firestore, 'users');
     // collectionData(col).subscribe(users => {
     //   this.users = users;
     // })
     
-    const config = {
-      projectId: 'dosomething-4154d',
-      appId: '1:114474360091:web:b643226a1d564543d307be',
-      databaseURL: 'https://dosomething-4154d.firebaseio.com',
-      storageBucket: 'dosomething-4154d.appspot.com',
-      locationId: 'europe-west',
-      apiKey: 'AIzaSyDqnLmY00Pg642iq0FExAihuzNYzRzqRr0',
-      authDomain: 'dosomething-4154d.firebaseapp.com',
-      messagingSenderId: '114474360091',
-      measurementId: 'G-BGHXZB0BNG',
-    }
-    const firebaseApp  = initializeApp(config);
-    const db = getFirestore();
+    // const config = {
+    //   projectId: 'dosomething-4154d',
+    //   appId: '1:114474360091:web:b643226a1d564543d307be',
+    //   databaseURL: 'https://dosomething-4154d.firebaseio.com',
+    //   storageBucket: 'dosomething-4154d.appspot.com',
+    //   locationId: 'europe-west',
+    //   apiKey: 'AIzaSyDqnLmY00Pg642iq0FExAihuzNYzRzqRr0',
+    //   authDomain: 'dosomething-4154d.firebaseapp.com',
+    //   messagingSenderId: '114474360091',
+    //   measurementId: 'G-BGHXZB0BNG',
+    // }
+    // const firebaseApp  = initializeApp(config);
+    // const db = getFirestore();
 
-    const querySnapshot = await getDocs(collection(db, "users"));
-    this.users = [];
-    querySnapshot.forEach((doc) => {
-      this.users.push(doc.data());
-      // console.log(`${doc.id} => ${doc.data()}`);
-    });
+    // const querySnapshot = await getDocs(collection(db, "users"));
+    // this.users = [];
+    // querySnapshot.forEach((doc) => {
+    //   this.users.push(doc.data());
+    //   // console.log(`${doc.id} => ${doc.data()}`);
+    // });
   }
 
 }
